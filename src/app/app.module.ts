3c468a64c9c4ca6c805a16718f057600104102dc
s1lv1u <silviu.bacila.adrian@gmail.com>
2025-03-08 16:50:57 +0200

    earch for product by category

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,33 @@ import { BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { ProductService } from './services/product.service';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { CommonModule } from '@angular/common';
 
+const routes: Routes = [
+  {path:'category/:id', component: ProductListComponent},
+  {path:'category', component: ProductListComponent},
+  {path:'products', component: ProductListComponent},
+  {path:'',redirectTo: '/products', pathMatch: 'full'},
+  {path:'**', redirectTo:'/products',pathMatch:'full'}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCategoryMenuComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
-  ],
-  providers: [
-  ],
+    HttpClientModule,
+    CommonModule ,
+ 
+],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,24 +1,41 @@
 import { Component } from '@angular/core';
 import { Product } from '../../common/product';
 import { ProductService } from '../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
+import e from 'express';
 
 @Component({
   selector: 'app-product-list',
-  standalone: false,
- // templateUrl: './product-list.component.html',
-  templateUrl: './product-list-grid.component.html',
+  standalone: false,  templateUrl: './product-list-grid.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
   products: Product[]=[];
+  currentCategoryId: number = 1;
 
-  constructor(private productService: ProductService){ }
+  constructor(private productService: ProductService, private route: ActivatedRoute){ }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
+  
   }
   listProducts() {
-    this.productService.getProductList().subscribe(data => {this. products=data});
+    // check if "id" parameter is available
+    
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    // get the "id" param string. convert string to a number using the "+" symbol
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }else{
+      // no category id available ... default to category id 1
+      this.currentCategoryId=1;
+    }
+
+    // now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(data => {this.products=data});
   }
 
 }
